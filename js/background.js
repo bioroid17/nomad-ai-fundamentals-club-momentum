e774cadcda6887dca699cb6547be69737af60a0f
bioroid17 <trace1997@ajou.ac.kr>
2026-06-04 22:26:00 +0900

    background loading animation

--- js/background.js
+++ js/background.js
@@ -4,5 +4,7 @@ const vw = window.innerWidth;
 const bgImage = document.createElement("img");
 bgImage.src = `https://picsum.photos/${vw}/${vh}`;
 bgImage.id = "background";
-
-document.body.appendChild(bgImage);
+bgImage.addEventListener("load", () => {
+  bgImage.style.animation = "appear 0.5s ease-in forwards";
+  document.body.appendChild(bgImage);
+});
